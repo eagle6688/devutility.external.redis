47f1626206eb7c04a37bbc8d3a9e8462013842af
Aldwin Su <fangyansu@163.com>
2018-04-16 23:45:48 +0800

    Update

--- src/main/java/devutility/external/redis/RedisStringHelper.java
+++ src/main/java/devutility/external/redis/RedisStringHelper.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import devutility.external.json.CompressUtils;
 import devutility.internal.dao.models.RedisInstance;
 import devutility.internal.lang.StringHelper;
+import devutility.internal.util.ArraysUtils;
 import devutility.internal.util.ListHelper;
 import redis.clients.jedis.Jedis;
 
@@ -179,6 +181,56 @@ public class RedisStringHelper extends RedisHelper {
 	}
 
 	/**
+	 * Set list data
+	 * @param key: Redis key
+	 * @param pageSize: Page size
+	 * @param list: Entities list
+	 * @param clazz: Object
+	 * @param expire: Expire time in seconds.
+	 * @return boolean
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws IOException
+	 * @throws InvocationTargetException
+	 */
+	public <T> boolean setList(String key, int pageSize, List<T> list, Class<T> clazz, int expire) throws IllegalArgumentException, IllegalAccessException, InvocationTargetException, IOException {
+		int pagesCount = getPagesCount(key);
+
+		if (pagesCount == 0) {
+			return setList(key, list, clazz, expire);
+		}
+
+		boolean result = true;
+
+		for (int index = 0; index < pagesCount; index++) {
+			List<T> listPage = ListHelper.paging(list, index, pageSize);
+			result = setList(key, listPage, clazz, expire);
+
+			if (!result) {
+				return result;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Set list data
+	 * @param key: Redis key
+	 * @param pageSize: Page size
+	 * @param list: Entities list
+	 * @param clazz: Object
+	 * @return boolean
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws IOException
+	 * @throws InvocationTargetException
+	 */
+	public <T> boolean setList(String key, int pageSize, List<T> list, Class<T> clazz) throws IllegalArgumentException, IllegalAccessException, InvocationTargetException, IOException {
+		return setList(key, pageSize, list, clazz, 0);
+	}
+
+	/**
 	 * Get list from Redis
 	 * @param key: Redis key
 	 * @param clazz: Class object
@@ -194,11 +246,63 @@ public class RedisStringHelper extends RedisHelper {
 	}
 
 	/**
+	 * Get list from Redis
+	 * @param key: Redis key
+	 * @param pageSize: Page size
+	 * @param clazz: Class object
+	 * @return {@code List<T>}
+	 * @throws IOException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 * @throws InvocationTargetException
+	 */
+	public <T> List<T> getList(String key, int pageSize, Class<T> clazz) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, IOException {
+		int pagesCount = getPagesCount(key);
+
+		if (pagesCount == 0) {
+			List<T> list = getList(key, clazz);
+
+			if (list != null) {
+				return list;
+			}
+
+			return new ArrayList<>();
+		}
+
+		List<T> list = new LinkedList<>();
+
+		for (int index = 0; index < pagesCount; index++) {
+			List<T> listPage = getList(key, pageSize, clazz);
+
+			if (listPage != null) {
+				list.addAll(listPage);
+			}
+		}
+
+		return new ArrayList<>(list);
+	}
+
+	/**
+	 * Set pages count
+	 * @param originalKey: Original key
+	 * @param count: Pages count
+	 * @return boolean
+	 */
+	private boolean setPagesCount(String originalKey, int count) {
+		if (StringHelper.isNullOrEmpty(originalKey) || count < 1) {
+			return false;
+		}
+
+		String key = pagesCountKey(originalKey);
+		return setInt(key, count);
+	}
+
+	/**
 	 * Get pages count
 	 * @param originalKey: Original key
 	 * @return int
 	 */
-	public int getPagesCount(String originalKey) {
+	private int getPagesCount(String originalKey) {
 		if (StringHelper.isNullOrEmpty(originalKey)) {
 			return 0;
 		}
@@ -207,40 +311,94 @@ public class RedisStringHelper extends RedisHelper {
 		return getInt(key);
 	}
 
+	/**
+	 * Set array page
+	 * @param originalKey: Original key
+	 * @param pageIndex: Page index
+	 * @param array: Array data
+	 * @return boolean
+	 * @throws IOException
+	 */
+	private boolean setArrayPage(String originalKey, int pageIndex, String[][] array) throws IOException {
+		String key = pagingDataKey(originalKey, pageIndex);
+		return setObject(key, array);
+	}
+
 	/***
-	 * Get page array
+	 * Get array page
 	 * @param originalKey: Original key
 	 * @param pageIndex: Page index
 	 * @return String[][]
 	 * @throws IOException
 	 */
-	public String[][] getPageArray(String originalKey, int pageIndex) throws IOException {
+	private String[][] getArrayPage(String originalKey, int pageIndex) throws IOException {
 		String key = pagingDataKey(originalKey, pageIndex);
 		return getObject(key, String[][].class);
 	}
 
-	public List<String[]> getAllPages(String originalKey) throws IOException {
-		List<String[]> list = new ArrayList<>();
+	/**
+	 * Set arrays data
+	 * @param originalKey: Original key
+	 * @param pageSize: Page size
+	 * @param array: Array data
+	 * @return boolean
+	 * @throws IOException
+	 */
+	public boolean setArrays(String originalKey, int pageSize, String[][] array) throws IOException {
+		if (StringHelper.isNullOrEmpty(originalKey) || pageSize == 0 || array == null) {
+			return false;
+		}
+
+		int pagesCount = calculatePagesCount(array.length, pageSize);
+
+		if (pagesCount == 1) {
+			return setObject(originalKey, array);
+		}
+
+		boolean result = true;
+
+		for (int index = 0; index < pagesCount; index++) {
+			String[][] arrayPage = ArraysUtils.pageArray(array, index, pageSize);
+			result = setArrayPage(originalKey, index, arrayPage);
+
+			if (!result) {
+				return result;
+			}
+		}
+
+		return setPagesCount(originalKey, pagesCount);
+	}
+
+	/**
+	 * Get arrays
+	 * @param originalKey: Original key
+	 * @param pageSize: Page size
+	 * @return String[][]
+	 * @throws IOException
+	 */
+	public String[][] getArrays(String originalKey, int pageSize) throws IOException {
 		int pagesCount = getPagesCount(originalKey);
 
 		if (pagesCount == 0) {
-			String[][] array = getObject(originalKey, String[][].class);
+			String[][] arrays = getObject(originalKey, String[][].class);
 
-			if (array == null) {
-				return new ArrayList<String[]>();
+			if (arrays != null) {
+				return arrays;
 			}
 
-			return Arrays.asList(array);
+			return new String[0][];
 		}
 
+		List<String[]> list = new LinkedList<>();
+
 		for (int index = 0; index < pagesCount; index++) {
-			String[][] array = getPageArray(originalKey, index);
+			String[][] array = getArrayPage(originalKey, index);
 
 			if (array != null) {
 				list.addAll(Arrays.asList(array));
 			}
 		}
 
-		return list;
+		return list.toArray(new String[0][]);
 	}
 }
