6139cc672ab412efa37e8c0310783630992e5fb9
Aldwin Su <fangyansu@163.com>
2019-10-12 23:57:46 +0800

    Implement enqueue in JedisListQueueProducer

--- src/main/java/devutility/external/redis/queue/list/JedisListQueueProducer.java
+++ src/main/java/devutility/external/redis/queue/list/JedisListQueueProducer.java
@@ -3,9 +3,16 @@ package devutility.external.redis.queue.list;
 import devutility.external.redis.queue.JedisQueueProducer;
 import redis.clients.jedis.Jedis;
 
+/**
+ * 
+ * JedisListQueueProducer
+ * 
+ * @author: Aldwin Su
+ * @version: 2019-10-12 23:54:18
+ */
 public class JedisListQueueProducer extends JedisQueueProducer {
 	@Override
 	public Object enqueue(Jedis jedis, String key, Object value) {
-		return null;
+		return jedis.lpush(key, convert(value));
 	}
 }
