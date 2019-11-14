3ef240998d456635ac04f4cdc4465927c0a40529
Aldwin Su <fangyansu@163.com>
2019-11-14 18:31:34 +0800

    Add QueueMode

---
+++ src/main/java/devutility/external/redis/com/QueueMode.java
@@ -0,0 +1,20 @@
+package devutility.external.redis.com;
+
+/**
+ * 
+ * QueueMode
+ * 
+ * @author: Aldwin Su
+ * @version: 2019-11-14 18:05:30
+ */
+public enum QueueMode {
+	/**
+	 * Point to point.
+	 */
+	P2P, 
+	
+	/**
+	 * 
+	 */
+	PUBSUB;
+}
